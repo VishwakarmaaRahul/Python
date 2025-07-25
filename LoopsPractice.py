7ec1ea16bfe64da5145c2d770eab94b303d0b7d7
Rahul Vishwakarma <rahul.vishwakarma@quadratyx.com>
2025-07-25 18:41:47 +0530

    remove comments

--- LoopsPractice.py
+++ LoopsPractice.py
@@ -1,38 +1,38 @@
 # working of for loops
 
-# for i in range(5):
-#     print(i)
-#
-# for _ in range(5):
-#     print("Hello")
+for i in range(5):
+    print(i)
+
+for _ in range(5):
+    print("Hello")
 
 
 # sum of all numbers from 1 to n using a loop
 
-# n = 5
-# result =0
-# for i in range(n):
-#     result = result + i
-#
-# print(result)
+n = 5
+result =0
+for i in range(n):
+    result = result + i
+
+print(result)
 
 
 # prints the multiplication table of a number n from 1 to 10.
 
-# n = 3
-# for i in range(1,11):
-#     print(n ,"*" ,i ,"=",n*i)
+n = 3
+for i in range(1,11):
+    print(n ,"*" ,i ,"=",n*i)
 
 
 # count how many vowels (a, e, i, o, u) a string contains
 
-# def count_vowels(text):
-#     vowels = "aeiou"
-#     count = 0
-#     for char in text.lower():
-#         if char in vowels:
-#             count += 1
-#     return count
-#
-# print(count_vowels("hello from quadratyx"))
+def count_vowels(text):
+    vowels = "aeiou"
+    count = 0
+    for char in text.lower():
+        if char in vowels:
+            count += 1
+    return count
+
+print(count_vowels("hello from quadratyx"))
 
