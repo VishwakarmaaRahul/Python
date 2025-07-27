bbd529d6912beb382502a4c710b4890bd603ca30
Rahul Sharma <rahulsharmados@gmail.com>
2025-07-27 19:07:08 +0530

    Add check Instance

--- Car.py
+++ Car.py
@@ -43,18 +43,18 @@ class ElectricCar(Car, Baterry, Engine):
 my_electric_car1 = ElectricCar("Audi","xyz","200 kWh","Desiel")
 my_electric_car2 = ElectricCar("Tesla", "Model S", "100 kWh", "Electric")
 
-# print(my_electric_car1.get__brand())
-# print(my_electric_car1.battery_size) 
-# print(my_electric_car1.engine_type)
-# print(f"The {my_electric_car1.get__brand()} car is {my_electric_car1.get_engine_type()} type with a battery size of {my_electric_car1.get_battery_size()}.")
+print(my_electric_car1.get__brand())
+print(my_electric_car1.battery_size) 
+print(my_electric_car1.engine_type)
+print(f"The {my_electric_car1.get__brand()} car is {my_electric_car1.get_engine_type()} type with a battery size of {my_electric_car1.get_battery_size()}.")
 
 electric_cars = [my_electric_car1, my_electric_car2]
 
 for car in electric_cars:
     print(f"Brand: {car.get__brand()}, Model: {car.model}, Battery Size: {car.battery_size}, Engine Type: {car.engine_type}")
 
-# print(isinstance(my_electric_car, ElectricCar))
-# print(isinstance(my_electric_car, Car))
+print(isinstance(my_electric_car1, ElectricCar))
+print(isinstance(my_electric_car1, Car))
 
 # my_electric_car.dispaly()
 # print(my_electric_car.get__brand())
@@ -73,6 +73,6 @@ print(Car.carCount)
 # print(c1.brand)
 # print(c1.model)
 
-# print(Car.general())  
+print(Car.general())  
 
 
